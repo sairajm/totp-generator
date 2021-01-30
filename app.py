647d3f4f0db02500c2a4c6fe786eb84b14ec5c7c
Sairaj Madhavan <techysai@gmail.com>
2021-01-30 16:50:34 -0700

    initial commit with time step logic

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from datetime import datetime, timezone
+import hashlib
+import hmac
+import base64
+app = Flask(__name__)
+
+time_step = 30 # time step in seconds
+t0 = 0
+
+@app.route('/generate')
+def generate_totp():
+    secretKey = request.args.get('code')
+    unix_utc_time = int(datetime.now(tz=timezone.utc).timestamp() * 1000)
+    number_of_time_steps =  ( unix_utc_time - t0 / time_step )
+    response = str(number_of_time_steps)
+    return response
+
+if __name__ == '__main__':
+    app.run()
