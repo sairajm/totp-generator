5b8f86cfc77fb7c9fb4e99b0c84a01d4439721bb
Sairaj Madhavan <techysai@gmail.com>
2021-01-30 17:04:17 -0700

    input validation and timestep calculation

--- app.py
+++ app.py
@@ -10,11 +10,24 @@ t0 = 0
 
 @app.route('/generate')
 def generate_totp():
-    secretKey = request.args.get('code')
-    unix_utc_time = int(datetime.now(tz=timezone.utc).timestamp())
-    number_of_time_steps =  ( unix_utc_time - t0 / time_step )
+    code_query_param = request.args.get('code')
+    if code_query_param is None:
+        return "Bad input, expected non-null or non-empty code.", 400
+
+    secretKey = str(code_query_param)
+
+    if "".__eq__(secretKey):
+        return "Bad input, expected non-null or non-empty code.", 400
+
+    number_of_time_steps =  get_number_of_time_steps()
     response = str(number_of_time_steps)
     return response
+
+def get_number_of_time_steps():
+    unix_utc_time = int(datetime.now(tz=timezone.utc).timestamp())
+    number_of_time_steps =  ( unix_utc_time - t0 / time_step )
+
+    return number_of_time_steps;
 
 if __name__ == '__main__':
     app.run()
